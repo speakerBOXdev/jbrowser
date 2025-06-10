72905b13fdbacd52ef77c8cf46aa1d8309567e7e
Josh <joshuawrose@gmail.com>
2025-06-10 13:27:26 -0400

    handle new lines for improved output

--- src/browser.py
+++ src/browser.py
@@ -15,26 +15,27 @@ class Browser:
         self.canvas.pack()
 
     def load(self, url):
-        # self.canvas.create_rectangle(10,20, 400, 300)
-        # self.canvas.create_oval(100,100,150,150)
-        # self.canvas.create_text(200,150, text="Hi!")
-
+        
         response=url.request()
         text=self.lex(response)
         cursor_x, cursor_y = HSTEP, VSTEP
         for c in text:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
+            if c == "\n":
+                cursor_y+=VSTEP
+                cursor_x=HSTEP
+            else:
+                self.canvas.create_text(cursor_x, cursor_y, text=c)
             cursor_x+=HSTEP
             if (cursor_x >= WIDTH-HSTEP):
                 cursor_x=HSTEP
                 cursor_y+=VSTEP
-
 
     def lex(self, body):
         text = ""
         in_tag = False
         in_entity = False
         entity=""
+        
         for c in body:
             if c == "<":
                 in_tag = True
@@ -47,7 +48,6 @@ class Browser:
                 in_entity = False
                 entity=entity+";"
                 entityValue=get_entity_val(entity)
-                #print(entityValue, end="")
                 text+=entityValue
                 entity=""
             elif not in_tag:
@@ -55,7 +55,6 @@ class Browser:
                     entity+=c
                 else:
                     text+=c
-                    #print(c, end="")
         return text
 
 class URL:
@@ -135,32 +134,6 @@ class URL:
         s.close()
 
         return content
-
-def show(body):
-    in_tag = False
-    in_entity = False
-    entity=""
-    for c in body:
-        if c == "<":
-            in_tag = True
-        elif c == ">":
-            in_tag = False
-        elif c == "&":
-            in_entity = True
-            entity="&"
-        elif c == ";":
-            in_entity = False
-            entity=entity+";"
-            entityValue=get_entity_val(entity)
-            print(entityValue, end="")
-            entity=""
-        elif not in_tag:
-            if in_entity:
-                entity+=c
-            else:
-                print(c, end="")
-
-    print()
 
 def get_entity_val(entity):
     if entity == "&lt;":
