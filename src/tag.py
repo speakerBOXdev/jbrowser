c3e2214967636beb470e0cbdbd0131f661e713a9
Josh <joshuawrose@gmail.com>
2025-06-11 22:15:37 -0400

    tag attr list; new line before h1

--- src/tag.py
+++ src/tag.py
@@ -1,11 +1,11 @@
 class Tag:
     def __init__(self, tag):
         self.tag=tag
+        self.attr=[]
         if " " in self.tag:
             self.tag, attributes = self.tag.split(" ", 1)
-        #print("Tag:{}".format(self.tag))
-
-    
+            for attr in attributes.split():
+                self.attr.append(attr)
 
 class Text:
     def __init__(self, text):
